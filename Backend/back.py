0a9702dcb37fa133f12c00333ef7117b3a86b6e9
Ramesh86-TurBo <pairamesh2002@gmail.com>
2023-08-11 01:08:48 +0530

    detail page and database added

--- Backend/back.py
+++ Backend/back.py
@@ -1,7 +1,8 @@
 # importing libraries
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 
 # creating app instance
 app = Flask(__name__)
@@ -18,7 +19,9 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=False, nullable=False)
-    task = db.Column(db.String(100), unique=False, nullable=False)
+    task = db.Column(db.Integer, unique=False, nullable=False)
+    cat = db.Column(db.String(100), unique=False, nullable=False)
+    note = db.Column(db.String(100), unique=False, nullable=False)
     date = db.Column(db.DateTime, default = datetime.utcnow)
  
     # repr method represents how one object of this datatable
@@ -41,27 +44,40 @@ def dashboard():
 
 
 # details page route
-@app.route('/detail')
+@app.route('/detail', methods = ['POST', 'GET'])
 def detail():
-    return render_template('detail.html', title = "details page")
+
+    if request.method == 'POST':
+
+        fname = request.form['fname']
+        ftask = request.form['ftask']
+        fnote = request.form['fnote']
+        fcat = request.form['fcat']
+
+        todo = Todo(name = fname, task = ftask, cat = fcat ,note = fnote)
+        db.session.add(todo)
+        db.session.commit()
+
+
+    allTodo = Todo.query.all()
+
+     # Check if the client accepts JSON response
+    if request.accept_mimetypes.best == 'application/json':
+        todo_list = [{"id": item.id, "name": item.name, "task": item.task,
+                      "cat": item.cat, "note": item.note, "date": item.date.strftime('%Y-%m-%d %H:%M:%S')}
+                     for item in allTodo]
+        return jsonify(todo_list)
+
+
+    return render_template('detail.html', title = "details page", allTodo = allTodo)
 
 
 # SELL PAGE ROUTES
 
 # sell/donate route page to create and read  the data
-@app.route('/sell', methods = ['POST', 'GET'])
+@app.route('/sell')
 def sell():
-    if request.method == 'POST':
-
-        fname = request.form['fname']
-        ftask = request.form['ftask']
-        todo = Todo(name = fname, task = ftask)
-        db.session.add(todo)
-        db.session.commit()
-
-    allTodo = Todo.query.all()
-
-    return render_template('sell.html', title = "sell/donate page", allTodo = allTodo)
+    return render_template('sell.html', title = "sell/donate page")
 
 # delete sell/donate page data route
 @app.route('/delete/<int:id>')
@@ -69,7 +85,7 @@ def delete(id):
     todo = Todo.query.filter_by(id = id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for('sell'))
+    return redirect(url_for('detail'))
 
 # update sell/donate page data route
 @app.route('/update/<int:id>', methods = ['POST', 'GET'])
@@ -83,7 +99,7 @@ def update(id):
         todo.task = ftask
         db.session.add(todo)
         db.session.commit()
-        return redirect(url_for('sell'))
+        return redirect(url_for('detail'))
 
     todo = Todo.query.filter_by(id = id).first()
 
